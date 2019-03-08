8c2da509dffcb29aaa3d3d46f940ba44a3e2ccf8
sitaramaraju21 <sitaramaraju21@gmail.com>
2019-03-08 08:09:12 -0500

    Add files via upload

---
+++ js/ChartsData.js
@@ -0,0 +1,89 @@
+var chartData = [
+  {
+    city: 'newyork',
+    data: [
+      {
+        date: '2018-01-01',
+        value: 25
+      },
+      {
+        date: '2018-02-01',
+        value: 30
+      },
+      {
+        date: '2018-03-01',
+        value: 45
+      },
+      {
+        date: '2018-04-01',
+        value: 53
+      },
+      {
+        date: '2018-05-01',
+        value: 25
+      },
+      {
+        date: '2018-06-01',
+        value: 33
+      }
+    ]
+  },
+  {
+    city: 'chicago',
+    data: [
+      {
+        date: '2018-01-01',
+        value: 35
+      },
+      {
+        date: '2018-02-01',
+        value: 18
+      },
+      {
+        date: '2018-03-01',
+        value: 35
+      },
+      {
+        date: '2018-04-01',
+        value: 40
+      },
+      {
+        date: '2018-05-01',
+        value: 55
+      },
+      {
+        date: '2018-06-01',
+        value: 12
+      }
+    ]
+  },
+  {
+    city: 'SFO',
+    data: [
+      {
+        date: '2018-01-01',
+        value: 43
+      },
+      {
+        date: '2018-02-01',
+        value: 47
+      },
+      {
+        date: '2018-03-01',
+        value: 15
+      },
+      {
+        date: '2018-04-01',
+        value: 21
+      },
+      {
+        date: '2018-05-01',
+        value: 38
+      },
+      {
+        date: '2018-06-01',
+        value: 28
+      }
+    ]
+  }
+];
